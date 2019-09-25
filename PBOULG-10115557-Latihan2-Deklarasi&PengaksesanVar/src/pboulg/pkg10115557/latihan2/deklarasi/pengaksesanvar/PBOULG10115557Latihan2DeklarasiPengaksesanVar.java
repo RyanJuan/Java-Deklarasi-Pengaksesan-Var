35a9c8ee4eefe071dc56e4b24ba53072cf441733
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:21:57 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan2-Deklarasi&PengaksesanVar/src/pboulg/pkg10115557/latihan2/deklarasi/pengaksesanvar/PBOULG10115557Latihan2DeklarasiPengaksesanVar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan2.deklarasi.pengaksesanvar;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan2
+ */
+public class PBOULG10115557Latihan2DeklarasiPengaksesanVar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    //Deklarasi var
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke var
+        nilaiInt = 75;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variable nilai = " + nilaiInt);
+        System.out.println("Isi Variable PHI = " + PHI);
+        System.out.println("Isi Variable logika = " + nilaiLogika);
+        System.out.println("isi Variable Karakter = " + nilaiKarakter);
+    }
+}
